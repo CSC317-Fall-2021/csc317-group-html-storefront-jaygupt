ee159aac6854d92b85c2c83b139f2aea9942bee7
Jay Gupta <jaygupt1973@gmail.com>
2021-10-30 18:59:58 -0700

    Add all Products to JSON object, and pull data into Main Product Area

--- js/main.js
+++ js/main.js
@@ -4,19 +4,211 @@ var data = {
       "name": "Captain America",
       "price": 15.78,
       "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
-      "imageURL": "images/product_images/action_figures/captain_america.jpeg"
+      "imageURL": "images/product_images/action_figures/captain_america.jpeg",
+      "category": "Action Figures"
     },
     {
       "name": "Batman",
       "price": 29.99,
       "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
-      "imageURL": "images/product_images/action_figures/batman.jpg"
+      "imageURL": "images/product_images/action_figures/batman.jpg",
+      "category": "Action Figures"
     },
     {
-      "name": "Spider Man",
+      "name": "Spider-Man",
       "price": 28.88,
       "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
-      "imageURL": "images/product_images/action_figures/spider_man.jpeg"
+      "imageURL": "images/product_images/action_figures/spider_man.jpeg",
+      "category": "Action Figures"
+    },
+    {
+      "name": "Superman",
+      "price": 25.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/action_figures/superman.jpg",
+      "category": "Action Figures"
+    },
+    {
+      "name": "Iron Man",
+      "price": 13.33,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/action_figures/iron_man.jpg",
+      "category": "Action Figures"
+    },
+    {
+      "name": "The Flash",
+      "price": 21.89,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/action_figures/the_flash.png",
+      "category": "Action Figures"
+    },
+    {
+      "name": "Chess",
+      "price": 25.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/board_games/chess.jpeg",
+      "category": "Board Games"
+    },
+    {
+      "name": "Chutes and Ladders",
+      "price": 9.39,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/board_games/chutes_and_ladders.jpg",
+      "category": "Board Games"
+    },
+    {
+      "name": "Labyrinth",
+      "price": 12.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/board_games/labyrinth.jpeg",
+      "category": "Board Games"
+    },
+    {
+      "name": "Life",
+      "price": 22.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/board_games/life.jpg",
+      "category": "Board Games"
+    },
+    {
+      "name": "Monopoly",
+      "price": 21.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/board_games/monopoly.jpeg",
+      "category": "Board Games"
+    },
+    {
+      "name": "Sorry",
+      "price": 26.49,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/board_games/sorry.jpg",
+      "category": "Board Games"
+    },
+    {
+      "name": "Alphabet Blocks",
+      "price": 18.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/building_blocks/alphabet_blocks.jpeg",
+      "category": "Building Blocks"
+    },
+    {
+      "name": "Legos",
+      "price": 20.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/building_blocks/legos.jpeg",
+      "category": "Building Blocks"
+    },
+    {
+      "name": "Lincoln Logs",
+      "price": 27.29,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/building_blocks/lincoln_logs.jpg",
+      "category": "Building Blocks"
+    },
+    {
+      "name": "Magnetic Tiles",
+      "price": 28.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/building_blocks/magnetic_tiles.jpg",
+      "category": "Building Blocks"
+    },
+    {
+      "name": "Pilo Blocks",
+      "price": 32.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/building_blocks/pilo_blocks.jpg",
+      "category": "Building Blocks"
+    },
+    {
+      "name": "Wooden Blocks",
+      "price": 14.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/building_blocks/wooden_blocks.jpeg",
+      "category": "Building Blocks"
+    },
+    {
+      "name": "Apples to Apples",
+      "price": 29.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cards/apples_to_apples.jpeg",
+      "category": "Cards"
+    },
+    {
+      "name": "Game of Thrones",
+      "price": 19.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cards/game_of_thrones.jpeg",
+      "category": "Cards"
+    },
+    {
+      "name": "Go Fish",
+      "price": 9.59,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cards/go_fish.jpg",
+      "category": "Cards"
+    },
+    {
+      "name": "Kings in the Corner",
+      "price": 18.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cards/kings_in_the_corner.jpg",
+      "category": "Cards"
+    },
+    {
+      "name": "Spot It",
+      "price": 14.59,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cards/spot_it.jpeg",
+      "category": "Cards"
+    },
+    {
+      "name": "Uno",
+      "price": 12.39,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cards/uno.jpeg",
+      "category": "Cards"
+    },
+    {
+      "name": "Hot Wheels",
+      "price": 19.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cars_and_motorcycles/hot_wheels.jpeg",
+      "category": "Cars and Motorcycles"
+    },
+    {
+      "name": "Lightning McQueen",
+      "price": 19.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cars_and_motorcycles/lightning_mcqueen.jpg",
+      "category": "Cars and Motorcycles"
+    },
+    {
+      "name": "Motorbike",
+      "price": 22.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cars_and_motorcycles/motorbike.jpeg",
+      "category": "Cars and Motorcycles"
+    },
+    {
+      "name": "RC Car",
+      "price": 39.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cars_and_motorcycles/rc_car.jpeg",
+      "category": "Cars and Motorcycles"
+    },
+    {
+      "name": "RC Motorcycle",
+      "price": 28.49,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cars_and_motorcycles/rc_motorcycle.jpeg",
+      "category": "Cars and Motorcycles"
+    },
+    {
+      "name": "Ride-On Car",
+      "price": 499.99,
+      "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore officiis consequuntur laborum ad dolorem veritatis tenetur praesentium saepe fugiat! Laboriosam laborum veniam architecto aliquam commodi adipisci doloribus, quis sapiente earum voluptatem quas voluptate fugiat! Error officiis odio praesentium dolorum?",
+      "imageURL": "images/product_images/cars_and_motorcycles/ride_on_car.jpeg",
+      "category": "Cars and Motorcycles"
     }
   ]
 }
