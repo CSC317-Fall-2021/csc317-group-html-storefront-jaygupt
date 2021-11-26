e999b5b13238c23e8af8ee5963f6844f02169418
Jay Gupta <jaygupt1973@gmail.com>
2021-11-25 21:14:43 -0800

    Revise Path to Favicon

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const port = 3000; // running on port 3000
 
 // assign public directory to serve static files through express
 app.use(express.static("public"));
-app.use(favicon(__dirname + "/public/favicon.ico"));
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon("./public/favicon.ico"));
 
 var products = require("./routes/products.js");
 app.use("/products", products);
